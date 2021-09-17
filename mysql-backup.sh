f5f8780e462bc1a6064acb573c7c9ec7b5118a58
root <root@ip-172-31-15-212.ap-south-1.compute.internal>
2021-09-17 05:45:38 +0000

    Jira abc102-Adding syntax to script

---
+++ mysql-backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###############################################
+#Created by Rahul
+#updated by Vinay on 17th sep 2021
+# -Printing message as per Jira ticket abc101
+# Syntax -sh mybackup-sh <DB_NameIn Caps>
+###############################################
+
+
+echo "Syntax to Run the script is .."
+echo "sh $ <DB_Name>"
+echo "provide mysql DB name to take backup..."
